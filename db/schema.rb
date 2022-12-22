57bae103edf12a24bfd312d5eedd7daff4f0e604
mutsumisadara <mutsumisadara@gmail.com>
2022-12-22 17:44:02 +0900

    add password secure n detail page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_22_075205) do
+ActiveRecord::Schema.define(version: 2022_12_22_081900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,9 @@ ActiveRecord::Schema.define(version: 2022_12_22_075205) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "string"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
