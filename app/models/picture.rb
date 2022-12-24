ffc13f9194eadaf454c3c009aceb65b1926ef464
mutsumisadara <mutsumisadara@gmail.com>
2022-12-24 11:09:23 +0900

    add favorite func

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ApplicationRecord
+  mount_uploader :picture, ImageUploader
+  validates :content,presence: true
   belongs_to :user
-  mount_uploader :image, ImageUploader
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#new'
+  resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :pictures do
