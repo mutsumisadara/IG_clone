0365ad303f59999bfce2d77036a0c75b3ae689e1
mutsumisadara <mutsumisadara@gmail.com>
2022-12-26 13:55:01 +0900

    add favorite list page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
 50.times do |n|
-    name = Faker::Games::Pokemon.name
-    email = Faker::Internet.email
-    password = "password"
-    User.create!(name: name,
-                 email: email,
-                 password: password,
-                 )
-  end
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name, email: email, password: password)
+end
